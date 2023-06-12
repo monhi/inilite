226ae828d0d6baa21643a48a4d8c524294bc027b
Monhi <m.monhi@gmail.com>
2023-06-12 09:12:55 +0330

    Update inilite.cpp

--- inilite.cpp
+++ inilite.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[])
 	//std::string		stemp = m_ini->getKeyValue(std::string("OMS"), std::string("ServerIP"));
 	m_ini->setKey("ADMIN", "USERNAME", "DATABASE");
 	m_ini->setKey("*****", "PASSWORD", "DATABASE");
-	std::string stemp = m_ini->getKeyValue("USERNAME", "DATABASE");
+	std::string stemp = m_ini->getKey("USERNAME", "DATABASE");
 	std::cout << "database user name is: " << stemp << std::endl;
 	delete			m_ini;
 	return			0;
